546742e004b59b5e4e1b0c4b02ace2b5fd52465b
j-klint <85409085+j-klint@users.noreply.github.com>
2023-03-13 02:21:52 +0200

    Fixed indentation and some (nearly) imperceptible bug(s).

--- source/sossotin/katakana.h
+++ source/sossotin/katakana.h
@@ -13,19 +13,22 @@ template<typename T, size_t N> constexpr size_t ArraySize(const T(&)[N]) { retur
 // Converts half-width katakana and a handful of other symbols from UCS-2/UTF-16 to 1602 printable values
 constexpr uint8_t UTF16_to_1602(uint16_t c)
 {
+	// C++11 allows only one return statement per constexpr function and no ifs.
+	// That's why we need to spam the ternary operator.
 	return (0xff61 <= c && c <= 0xff9f) ? (c - 0xff61 + 0b10100001) : //ascii lookalikes
 	       (0xff01 <= c && c <= 0xff5d) ? (c - 0xff61 + 0b10100001) : //katakana proper
 	       (c == 0x4e07)                ? 0b11111011 :                //man
 	       (c == 0x5186)                ? 0b11111100 :                //en
 	       (c == 0x5343)                ? 0b11111010 :                //sen
 	       (c == 0xffe5)                ? 0b01011100 :                //Y-yen
-	       (c == 0xffe9)                ? 0b10001111 :                //vas. nuoli
-	       (c == 0xffeb)                ? 0b10001110 :                //oik. nuoli
+	       (c == 0xffe9)                ? 0b01111111 :                //vas. nuoli
+	       (c == 0xffeb)                ? 0b01111110 :                //oik. nuoli
 	       (c == 0x00c4 || c == 0x00e4) ? 0b11100001 :                //Ää
 	       (c == 0x00d6 || c == 0x00f6) ? 0b11101111 :                //Öö
 	       (c == 0x00dc || c == 0x00fc) ? 0b11110101 :                //Üü
 	       (c == 0x00d1 || c == 0x00f1) ? 0b11101110 :                //Ññ
-         (c == L'´')                  ? '\''       :
+	       (c == L'É'   || c == L'é'  ) ? 'E'        :                //Éé, LCD screen doesn't have this symbol. //TODO: draw an É symbol for the LCD display to display
+	       (c == L'´')                  ? '\''       :
 	       (c < 128)                    ? c          :                //non-ASCII chars could be falsely interpreted as katakana
 	       0xff;
 }
--- source/sossotin/morse.cpp
+++ source/sossotin/morse.cpp
@@ -4,27 +4,29 @@
 
 namespace
 {
-  class MorseTreeNode
-  {
-  public:
-    uint8_t letter;
-    uint8_t left, right;
-    uint8_t operator[](Boop b) const
-    {
-    if ( b == Boop::dit )
-      return left;
-    else
-      return right;
-    }
-  };
+	class MorseTreeNode
+	{
+	public:
+		uint8_t letter;
+		uint8_t left, right;
+		uint8_t operator[](Boop b) const
+		{
+			if ( b == Boop::dit )
+				return left;
+			else
+				return right;
+		}
+	};
 
-  struct WabunCode
-  {
-    uint8_t kana;
-    uint8_t code, len;
-  };
+	struct WabunCode
+	{
+		uint8_t kana;
+		uint8_t code, len;
+	};
 
-  constexpr MorseTreeNode MorseTreeData[] PROGMEM {
+
+
+	constexpr MorseTreeNode MorseTreeData[] PROGMEM {
 		{  0,  1, 2 },
 		{'E',  3, 4 },
 		{'T', 31,32 },
@@ -39,8 +41,8 @@ namespace
 		{'5',  0, 0 },
 		{'4',  0, 0 },
 		{'3',  0, 0 },
-		{'E',  0, 0 },
-		{0xff,17, 0 },//Eth
+		{'E',  0, 0 }, //É, LCD doesn't have this symbol by default //TODO: draw an É symbol for the LCD display to display
+		{0xff,17, 0 }, //Eth
 		{'2',  0, 0 },
 		{'?',  0, 0 },
 		{'R', 20,21 },
@@ -92,7 +94,7 @@ namespace
 
 	constexpr MorseCode morseCodes[] PROGMEM {
 		{6,0b010010} // \"
-		,{0,0}//#-
+		,{0,0}//#
 		,{0,0}//$
 		,{0,0}//%
 		,{0,0}//&
@@ -156,83 +158,83 @@ namespace
 		,{6,0b011110}//\'
 	};
 
-  constexpr WabunCode wabunCodes[] PROGMEM {
-    {UTF16_to_1602(L'ｧ'),  0b11011,  5},
-    {UTF16_to_1602(L'ｨ'),  0b01,     2},
-    {UTF16_to_1602(L'ｩ'),  0b001,    3},
-    {UTF16_to_1602(L'ｪ'),  0b10111,  5},
-    {UTF16_to_1602(L'ｫ'),  0b01000,  5},
-    {UTF16_to_1602(L'ｬ'),  0b011,    3},
-    {UTF16_to_1602(L'ｭ'),  0b10011,  4},
-    {UTF16_to_1602(L'ｮ'),  0b11,     2},
-    {UTF16_to_1602(L'ｯ'),  0b0110,   4},
-    {UTF16_to_1602(L'ﾑ'),  0b1,      1},
-    {UTF16_to_1602(L'ﾍ'),  0b0,      1},
-    {UTF16_to_1602(L'ｲ'),  0b01,     2},
-    {UTF16_to_1602(L'ﾀ'),  0b10,     2},
-    {UTF16_to_1602(L'ﾖ'),  0b11,     2},
-    {UTF16_to_1602(0xff9e),0b00,     2}, //hipsut "dakuten"
-    {UTF16_to_1602(L'ｳ'),  0b001,    3},
-    {UTF16_to_1602(L'ﾅ'),  0b010,    3},
-    {UTF16_to_1602(L'ﾎ'),  0b100,    3},
-    {UTF16_to_1602(L'ﾔ'),  0b011,    3},
-    {UTF16_to_1602(L'ﾗ'),  0b000,    3},
-    {UTF16_to_1602(L'ﾘ'),  0b110,    3},
-    {UTF16_to_1602(L'ﾚ'),  0b111,    3},
-    {UTF16_to_1602(L'ﾜ'),  0b101,    3},
-    {UTF16_to_1602(L'ｶ'),  0b0100,   4},
-    {UTF16_to_1602(L'ｸ'),  0b0001,   4},
-    {UTF16_to_1602(L'ｹ'),  0b1011,   4},
-    {UTF16_to_1602(L'ｺ'),  0b1111,   4},
-    {UTF16_to_1602(L'ｿ'),  0b1110,   4},
-    {UTF16_to_1602(L'ﾁ'),  0b0010,   4},
-    {UTF16_to_1602(L'ﾂ'),  0b0110,   4},
-    {UTF16_to_1602(L'ﾆ'),  0b1010,   4},
-    {UTF16_to_1602(L'ﾇ'),  0b0000,   4},
-    {UTF16_to_1602(L'ﾈ'),  0b1101,   4},
-    {UTF16_to_1602(L'ﾉ'),  0b0011,   4},
-    {UTF16_to_1602(L'ﾊ'),  0b1000,   4},
-    {UTF16_to_1602(L'ﾌ'),  0b1100,   4},
-    {UTF16_to_1602(L'ﾏ'),  0b1001,   4},
-    {UTF16_to_1602(L'ﾕ'),  0b10011,  4},
-    {UTF16_to_1602(L'ﾛ'),  0b0101,   4},
-    {UTF16_to_1602(L'ｦ'),  0b0111,   4},
-    {UTF16_to_1602(L'ﾋ'),  0b11001,  5},
-    {UTF16_to_1602(L'ﾝ'),  0b01010,  5},
-    {UTF16_to_1602(L'ｱ'),  0b11011,  5},
-    {UTF16_to_1602(L'ｴ'),  0b10111,  5},
-    {UTF16_to_1602(L'ｵ'),  0b01000,  5},
-    {UTF16_to_1602(L'ｷ'),  0b10100,  5},
-    {UTF16_to_1602(L'ｻ'),  0b10101,  5},
-    {UTF16_to_1602(L'ｼ'),  0b11010,  5},
-    {UTF16_to_1602(L'ｽ'),  0b11101,  5},
-    {UTF16_to_1602(L'ｾ'),  0b01110,  5},
-    {UTF16_to_1602(L'ﾃ'),  0b01011,  5},
-    {UTF16_to_1602(L'ﾄ'),  0b00100,  5},
-    {UTF16_to_1602(L'ﾕ'),  0b10011,  5},
-    {UTF16_to_1602(L'ﾐ'),  0b00101,  5},
-    {UTF16_to_1602(L'ﾒ'),  0b10001,  5},
-    {UTF16_to_1602(L'ﾓ'),  0b10010,  5},
-    {UTF16_to_1602(L'ﾙ'),  0b10110,  5},
-    {UTF16_to_1602(0xff9f),0b00110,  5}, //pallero "handakuten"
-    {UTF16_to_1602(L'ｰ'),  0b01101,  5},
-    {                 14,  0b00010,  5}, //shift out, international mode
-    {                '0',  0b11111,  5}, //0
-    {                '1',  0b01111,  5},
-    {                '2',  0b00111,  5},
-    {                '3',  0b00011,  5},
-    {                '4',  0b00001,  5},
-    {                '5',  0b00000,  5},
-    {                '6',  0b10000,  5},
-    {                '7',  0b11000,  5},
-    {                '8',  0b11100,  5},
-    {                '9',  0b11110,  5}, //9
-    {UTF16_to_1602(L'､'),  0b010101, 6},
-    {UTF16_to_1602(L'｡'),  0b010100, 6},
-    {UTF16_to_1602(L'('),  0b101101, 6},
-    {UTF16_to_1602(L')'),  0b010010, 6},
-    {                 15,  0b100111, 6}  //shift in,  wabun mode
-  };	
+	constexpr WabunCode wabunCodes[] PROGMEM {
+		{UTF16_to_1602(L'ｧ'),  0b11011,  5},
+		{UTF16_to_1602(L'ｨ'),  0b01,     2},
+		{UTF16_to_1602(L'ｩ'),  0b001,    3},
+		{UTF16_to_1602(L'ｪ'),  0b10111,  5},
+		{UTF16_to_1602(L'ｫ'),  0b01000,  5},
+		{UTF16_to_1602(L'ｬ'),  0b011,    3},
+		{UTF16_to_1602(L'ｭ'),  0b10011,  4},
+		{UTF16_to_1602(L'ｮ'),  0b11,     2},
+		{UTF16_to_1602(L'ｯ'),  0b0110,   4},
+		{UTF16_to_1602(L'ﾑ'),  0b1,      1},
+		{UTF16_to_1602(L'ﾍ'),  0b0,      1},
+		{UTF16_to_1602(L'ｲ'),  0b01,     2},
+		{UTF16_to_1602(L'ﾀ'),  0b10,     2},
+		{UTF16_to_1602(L'ﾖ'),  0b11,     2},
+		{UTF16_to_1602(0xff9e),0b00,     2}, //hipsut "dakuten"
+		{UTF16_to_1602(L'ｳ'),  0b001,    3},
+		{UTF16_to_1602(L'ﾅ'),  0b010,    3},
+		{UTF16_to_1602(L'ﾎ'),  0b100,    3},
+		{UTF16_to_1602(L'ﾔ'),  0b011,    3},
+		{UTF16_to_1602(L'ﾗ'),  0b000,    3},
+		{UTF16_to_1602(L'ﾘ'),  0b110,    3},
+		{UTF16_to_1602(L'ﾚ'),  0b111,    3},
+		{UTF16_to_1602(L'ﾜ'),  0b101,    3},
+		{UTF16_to_1602(L'ｶ'),  0b0100,   4},
+		{UTF16_to_1602(L'ｸ'),  0b0001,   4},
+		{UTF16_to_1602(L'ｹ'),  0b1011,   4},
+		{UTF16_to_1602(L'ｺ'),  0b1111,   4},
+		{UTF16_to_1602(L'ｿ'),  0b1110,   4},
+		{UTF16_to_1602(L'ﾁ'),  0b0010,   4},
+		{UTF16_to_1602(L'ﾂ'),  0b0110,   4},
+		{UTF16_to_1602(L'ﾆ'),  0b1010,   4},
+		{UTF16_to_1602(L'ﾇ'),  0b0000,   4},
+		{UTF16_to_1602(L'ﾈ'),  0b1101,   4},
+		{UTF16_to_1602(L'ﾉ'),  0b0011,   4},
+		{UTF16_to_1602(L'ﾊ'),  0b1000,   4},
+		{UTF16_to_1602(L'ﾌ'),  0b1100,   4},
+		{UTF16_to_1602(L'ﾏ'),  0b1001,   4},
+		{UTF16_to_1602(L'ﾕ'),  0b10011,  4},
+		{UTF16_to_1602(L'ﾛ'),  0b0101,   4},
+		{UTF16_to_1602(L'ｦ'),  0b0111,   4},
+		{UTF16_to_1602(L'ﾋ'),  0b11001,  5},
+		{UTF16_to_1602(L'ﾝ'),  0b01010,  5},
+		{UTF16_to_1602(L'ｱ'),  0b11011,  5},
+		{UTF16_to_1602(L'ｴ'),  0b10111,  5},
+		{UTF16_to_1602(L'ｵ'),  0b01000,  5},
+		{UTF16_to_1602(L'ｷ'),  0b10100,  5},
+		{UTF16_to_1602(L'ｻ'),  0b10101,  5},
+		{UTF16_to_1602(L'ｼ'),  0b11010,  5},
+		{UTF16_to_1602(L'ｽ'),  0b11101,  5},
+		{UTF16_to_1602(L'ｾ'),  0b01110,  5},
+		{UTF16_to_1602(L'ﾃ'),  0b01011,  5},
+		{UTF16_to_1602(L'ﾄ'),  0b00100,  5},
+		{UTF16_to_1602(L'ﾕ'),  0b10011,  5},
+		{UTF16_to_1602(L'ﾐ'),  0b00101,  5},
+		{UTF16_to_1602(L'ﾒ'),  0b10001,  5},
+		{UTF16_to_1602(L'ﾓ'),  0b10010,  5},
+		{UTF16_to_1602(L'ﾙ'),  0b10110,  5},
+		{UTF16_to_1602(0xff9f),0b00110,  5}, //pallero "handakuten"
+		{UTF16_to_1602(L'ｰ'),  0b01101,  5},
+		{                 14,  0b00010,  5}, //shift out, international mode
+		{                '0',  0b11111,  5}, //0
+		{                '1',  0b01111,  5},
+		{                '2',  0b00111,  5},
+		{                '3',  0b00011,  5},
+		{                '4',  0b00001,  5},
+		{                '5',  0b00000,  5},
+		{                '6',  0b10000,  5},
+		{                '7',  0b11000,  5},
+		{                '8',  0b11100,  5},
+		{                '9',  0b11110,  5}, //9
+		{UTF16_to_1602(L'､'),  0b010101, 6},
+		{UTF16_to_1602(L'｡'),  0b010100, 6},
+		{UTF16_to_1602(L'('),  0b101101, 6},
+		{UTF16_to_1602(L')'),  0b010010, 6},
+		{                 15,  0b100111, 6}  //shift in,  wabun mode
+	};
 
 	constexpr uint8_t numWabuns = ArraySize(wabunCodes);
 }
@@ -245,16 +247,16 @@ uint8_t MorseTree::read() const
 	if ( curr == 0 )
 		return 0xff;
 
-  uint8_t retval;
-  memcpy_P(&retval, &MorseTreeData[curr].letter, sizeof(retval));
-  return retval;
+	uint8_t retval;
+	memcpy_P(&retval, &MorseTreeData[curr].letter, sizeof(retval));
+	return retval;
 }
 
 bool MorseTree::step(Boop b)
 {
 	++codeLength;
-  MorseTreeNode tempNode;
-  memcpy_P(&tempNode, MorseTreeData+curr, sizeof(tempNode));
+	MorseTreeNode tempNode;
+	memcpy_P(&tempNode, MorseTreeData + curr, sizeof(tempNode));
 	curr = tempNode[b];
 	return curr != 0;
 }
@@ -266,8 +268,8 @@ MorseCode MorseEncode(uint8_t ltr)
 		for ( int i = 0; i < numWabuns; ++i )
 		{
 			WabunCode code;
-      memcpy_P(&code, wabunCodes + i, sizeof(code));
-      if ( code.kana == ltr )
+			memcpy_P(&code, wabunCodes + i, sizeof(code));
+			if ( code.kana == ltr )
 				return { code.len, code.code };
 		}
 		return { 0,0 };
@@ -280,6 +282,7 @@ MorseCode MorseEncode(uint8_t ltr)
 	case UTF16_to_1602(L'ä'): return { 4,0b0101 };
 	case UTF16_to_1602(L'ö'): return { 4,0b1110 };
 	case UTF16_to_1602(L'ü'): return { 4,0b0011 };
+	//case UTF16_to_1602(L'é'): return { 1,0b0    }; //TODO: draw an É symbol for the LCD display to display
 	}
 
 	if ( ltr < '\"' || ltr >= '{' )
@@ -288,9 +291,9 @@ MorseCode MorseEncode(uint8_t ltr)
 	if ( 'a' <= ltr && ltr <= 'z' )
 		ltr -= 'a' - 'A';
 
-  MorseCode retval;
-  memcpy_P(&retval, morseCodes + (ltr - '\"'), sizeof(retval));
-  return retval;
+	MorseCode retval;
+	memcpy_P(&retval, morseCodes + (ltr - '\"'), sizeof(retval));
+	return retval;
 }
 
 bool WabunDecoder::step(Boop b)
@@ -314,7 +317,7 @@ bool WabunDecoder::step(Boop b)
 		memcpy_P(&tempCode, &wabunCodes[index], sizeof(tempCode));
 
 		if ( tempCode.len > codeLength ) break;
-			
+
 		if ( tempCode.len == codeLength && tempCode.code == code )
 		{
 			curr = index;
@@ -342,8 +345,8 @@ void WabunDecoder::reset() { init(); }
 
 uint8_t WabunDecoder::read() const
 {
-  WabunCode tempCode;
-  memcpy_P(&tempCode, &wabunCodes[curr], sizeof(tempCode));
+	WabunCode tempCode;
+	memcpy_P(&tempCode, &wabunCodes[curr], sizeof(tempCode));
 
 	if ( tempCode.code == code && tempCode.len == codeLength )
 		return tempCode.kana;
